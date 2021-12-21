843ce650a15d852501d75484d22e4853ae4f3ac6
jebincvarghese <43873109+jebincvarghese@users.noreply.github.com>
2021-12-21 14:17:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-   'use strict';
-   var express = require('express');
-   var app = express();
-   app.get('/', function (req, res) {
-     res.send('<h1><center>Node.js Application : v1</h1></center>');
-   });
-   app.listen(3000, function () {
-     console.log('Example app listening on port 3000!');
-   });
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 
